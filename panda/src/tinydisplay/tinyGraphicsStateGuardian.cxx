f0bf4b8417b1c7772756648f7e7115b80275d425
rdb <git@rdb.name>
2022-12-26 19:25:53 +0100

    display: Add support for asynchronous screenshot download

--- panda/src/tinydisplay/tinyGraphicsStateGuardian.cxx
+++ panda/src/tinydisplay/tinyGraphicsStateGuardian.cxx
@@ -1393,8 +1393,8 @@ framebuffer_copy_to_texture(Texture *tex, int view, int z,
  */
 bool TinyGraphicsStateGuardian::
 framebuffer_copy_to_ram(Texture *tex, int view, int z,
-                        const DisplayRegion *dr,
-                        const RenderBuffer &rb) {
+                        const DisplayRegion *dr, const RenderBuffer &rb,
+                        ScreenshotRequest *request) {
   nassertr(tex != nullptr && dr != nullptr, false);
 
   int xo, yo, w, h;
